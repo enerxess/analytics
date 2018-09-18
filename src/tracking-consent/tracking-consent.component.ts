11cf28407b6b8696766a4783e09e43955b12e04a
D2KX <dennis.martin.herbers@gmail.com>
2018-09-18 17:28:19 +0200

    v1.0.10

--- src/tracking-consent/tracking-consent.component.ts
+++ src/tracking-consent/tracking-consent.component.ts
@@ -1,57 +1,22 @@
-import {
-  Component,
-  Inject,
-  OnInit,
-  PLATFORM_ID,
-  Renderer2,
-  ViewEncapsulation
-} from '@angular/core';
-import { DOCUMENT, isPlatformBrowser } from '@angular/common';
-import {
-  MAT_RIPPLE_GLOBAL_OPTIONS,
-  RippleGlobalOptions
-} from '@angular/material';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { ConfigService } from '../lib/config.service';
 import { AnalyticsConfig } from '../lib/analytics.service';
 import { fadeInAnimation } from '../lib/animations';
 
-const globalRippleConfig: RippleGlobalOptions = { disabled: true };
-
 @Component({
   selector: 'exs-tracking-consent',
   templateUrl: './tracking-consent.component.html',
   styleUrls: ['./tracking-consent.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers: [
-    { provide: MAT_RIPPLE_GLOBAL_OPTIONS, useValue: globalRippleConfig }
-  ],
   animations: [fadeInAnimation]
 })
-export class TrackingConsentComponent implements OnInit {
+export class TrackingConsentComponent {
   analyticsConfig$: Observable<Partial<AnalyticsConfig>>;
 
-  constructor(
-    @Inject(PLATFORM_ID) private _platformId: Object,
-    @Inject(DOCUMENT) private _document: any,
-    private _renderer: Renderer2,
-    private _configService: ConfigService
-  ) {
+  constructor(private _configService: ConfigService) {
     this.analyticsConfig$ = this._configService.analyticsConfig$;
-  }
-
-  ngOnInit(): void {
-    this.appendFont();
-  }
-
-  private appendFont(): void {
-    if (isPlatformBrowser(this._platformId)) {
-      const roboto = this._renderer.createElement('link');
-      roboto.href = 'https://fonts.googleapis.com/css?family=Roboto:400,500';
-      roboto.rel = 'stylesheet';
-      this._renderer.appendChild(this._document.head, roboto);
-    }
   }
 
   changeConsentState(consent: boolean): void {
