aee2f20d7b3b4363508397da713f2c1b94798210
D2KX <dennis.martin.herbers@gmail.com>
2018-09-18 17:38:52 +0200

    1.0.11

--- src/tracking-consent/tracking-consent.component.ts
+++ src/tracking-consent/tracking-consent.component.ts
@@ -1,16 +1,23 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Observable } from 'rxjs';
 
 import { ConfigService } from '../lib/config.service';
 import { AnalyticsConfig } from '../lib/analytics.service';
-import { fadeInAnimation } from '../lib/animations';
 
 @Component({
   selector: 'exs-tracking-consent',
   templateUrl: './tracking-consent.component.html',
   styleUrls: ['./tracking-consent.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  animations: [fadeInAnimation]
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.2s 2s ease-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class TrackingConsentComponent {
   analyticsConfig$: Observable<Partial<AnalyticsConfig>>;
